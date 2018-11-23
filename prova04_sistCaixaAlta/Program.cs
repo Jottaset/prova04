842621ab79ce2b14b6953d7f443a8166cf84dd70
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-11-22 20:48:39 -0400

    questao 4 da prova 1

---
+++ prova04_sistCaixaAlta/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace prova04_sistCaixaAlta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nome;
+            Console.WriteLine("Informe o Nome Completo:");
+            Nome = Console.ReadLine();
+            Nome = Nome.ToUpper();
+
+            Console.WriteLine(Nome);
+        }
+    }
+}
